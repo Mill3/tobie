336305a314427d5f82e7d491689d190d84082cae
Antoine Girard <thetoine@gmail.com>
2018-07-31 16:48:32 -0400

    source url

--- src/components/reel/Reel.js
+++ src/components/reel/Reel.js
@@ -7,11 +7,12 @@ import ReelPlayer from './ReelPlayer'
 class Reel extends Component {
 
   render() { 
+    console.log(this.props.data);
     return (
       <ReactCursorPosition>
         <ReelPlayer 
           video_preview_src={this.props.data.acf.text_video_preview ? this.props.data.acf.text_video_preview.source_url : null}
-          video_full_src={this.props.data.acf.text_video_full ? this.props.data.acf.text_video_full.text_source_url : null}
+          video_full_src={this.props.data.acf.text_video_full ? this.props.data.acf.text_video_full.source_url : null}
         />
       </ReactCursorPosition>
     );
