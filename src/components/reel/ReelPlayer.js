8454f8eb07f1138c9d36116c684d41d4f3cf9ebc
Antoine Girard <thetoine@gmail.com>
2018-08-06 16:13:22 -0400

    add alert

--- src/components/reel/ReelPlayer.js
+++ src/components/reel/ReelPlayer.js
@@ -36,7 +36,7 @@ class ReelPlayer extends React.Component {
       // }, 250);
 
       this.refs.player.video.handleCanPlayThrough = () => {
-        // alert("Can play through video without stopping");
+        alert("Can play through video without stopping");
         this.refs.player.play()
       };
       
