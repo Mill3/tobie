92d3f052b039f885b7bb3a13f851218132379333
Antoine Girard <thetoine@gmail.com>
2018-09-11 10:13:01 -0400

    ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,16 +1,10 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { renderToString } from 'react-dom/server'
 
 import createStoreMethod from './src/store'
 
 let store = createStoreMethod()
 
-export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
-  const ConnectedBody = () => (
-    <Provider store={store}>
-      {bodyComponent}
-    </Provider>
-  )
-  replaceBodyHTMLString(renderToString(<ConnectedBody/>))
-}
+const ConnectedRootElement = ({ element }) => <Provider store={store}>{element}</Provider>
+
+export const wrapRootElement = ConnectedRootElement
