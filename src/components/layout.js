be95f1561184fa9bceda88ec5c69cc2bf3997ed7
Antoine Girard <thetoine@gmail.com>
2023-01-10 14:48:33 -0500

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -76,6 +76,13 @@ const Layout = ({ children, location, inverted, hideHeader }) => (
           <meta name="msapplication-TileColor" content="#000000" />
           <meta name="theme-color" content="#000000" />
           <meta name="ahrefs-site-verification" content="60731af8a710220fa57373aeca408f14e0fb65e8dc1322d66fb4e2c5621f9da1" />
+          <!-- Google Tag Manager -->
+          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NLDWMWJ');</script>
+          <!-- End Google Tag Manager -->
         </Helmet>    
         <div className={
           classNames({ 
