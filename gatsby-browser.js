c93ba1bc65bfb8db37147e02c91d6f1fc7b9732b
Antoine Girard <antoine@mill3.studio>
2026-04-03 13:21:11 -0400

    fix: replace gatsby push with navigate in gatsby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,18 +1,9 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
 import React from 'react'
-import { Router } from 'react-router-dom'
+import { navigate } from 'gatsby'
 import { Provider } from 'react-redux'
-import { push } from 'gatsby'
 
 import createStoreMethod from './src/store'
-
 import detectLocale from '@utils/detect-locale'
-import { setLocale } from '@reducers/actions'
 
 let store = createStoreMethod()
 
@@ -23,15 +14,11 @@ export const wrapRootElement = ConnectedRootElement
 export const onInitialClientRender = () => {
   window.___GATSBYGRAM_INITIAL_RENDER_COMPLETE = true
 
-  let root = '/'
-  let detectedLocale = detectLocale()
+  const detectedLocale = detectLocale()
 
-  // dispatch detected locale
   store.dispatch({ type: 'SET_LOCALE', locale: detectedLocale })
 
-  // on root only, redirect
-  if (window.location.pathname === root) {
-    push(`${detectedLocale}/`)
+  if (window.location.pathname === '/') {
+    navigate(`/${detectedLocale}/`)
   }
-
 }
